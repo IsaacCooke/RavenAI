7be4779efd4c3dada04175c12057c87ad62ca4d8
Azrael-Studios <cookeisaac2@gmail.com>
2022-11-12 10:38:50 +0000

    Restarted API

--- RavenBack/Models/RavenDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace RavenBack.Models
-{
-    public class RavenDatabaseSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string UsersCollectionName { get; set; } = null!;
-    }
-}
