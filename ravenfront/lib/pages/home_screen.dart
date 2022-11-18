5a01556c72b4214885b5668864b41e1493172879
Isaac <100490799+Azrael-Studios@users.noreply.github.com>
2022-11-18 12:35:52 +0000

    Started home_screen.dart

---
+++ ravenfront/lib/pages/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Home extends StatefulWidget{
+  super({ key: key });
+
+  _HomeState createState() => Home;
+}
+
+class _HomeState extends State<Home>{
+  
+}
